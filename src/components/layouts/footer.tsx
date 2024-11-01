ddeb3671e6864984047d91b5a9630b5ec6daf3a8
Harry Madison (Trinh Cuong) <harrymadison297@gmail.com>
2024-11-01 11:13:17 +0700

    Update footer.tsx

--- src/components/layouts/footer.tsx
+++ src/components/layouts/footer.tsx
@@ -23,7 +23,7 @@ const Footer = () => {
         ))}
       </div>
       <div className='mt-20 flex items-center justify-between text-sm'>
-        <div>&copy; {new Date().getFullYear()} Hong</div>
+        <div>&copy; {new Date().getFullYear()} Harry</div>
         <Link
           href='https://github.com/harrymadison297/harry-portfolio'
           className='flex items-center justify-center overflow-hidden rounded-md border'
